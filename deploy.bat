d930b2661c029f152c19d9e65f13d42000d673ba
Rusaln-IT <ruslan.r.f@yandex.ru>
2025-11-30 16:34:39 +0500

    commit admin panel category

---
+++ deploy.bat
@@ -0,0 +1,31 @@
+@echo off
+echo 🚀 Автодеплой запущен...
+
+REM Ожидание завершения каждой команды
+setlocal enabledelayedexpansion
+
+REM Добавляем изменения и пушим
+echo 📦 Добавляем изменения в git...
+git add .
+git commit -m "commit admin panel category"
+git push origin master
+
+REM Сборка проекта с ожиданием завершения
+echo ⚙️ Сборка проекта...
+call npm run build
+
+REM Проверяем успешность сборки
+if %errorlevel% neq 0 (
+    echo ❌ Ошибка сборки! Код ошибки: %errorlevel%
+    pause
+    exit /b
+)
+
+REM Добавляем сборку в git
+echo 📦 Добавляем сборку в git...
+git add -f public/build/
+git commit -m "Build assets for production"
+git push origin master
+
+echo ✅ Локальный деплой завершен
+pause
---
+++ deploy_server.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+#cd /home/l/lionti9k/webway.live  # путь к проекту на Beget
+
+# Подтягиваем изменения из GitHub
+git pull origin master
+
+echo "✅ Сервер обновлён"
+
+php8.3 composer.phar install --no-dev --optimize-autoloader
+
+php8.3 artisan cache:clear        # Очистить общий кэш
+php8.3 artisan config:clear       # Очистить кеш конфигураций
+php8.3 artisan route:clear        # Очистить кеш маршрутов
+php8.3 artisan view:clear         # Очистить кеш Blade шаблонов
+php8.3 artisan event:clear        # Очистить кеш событий (если используется)
+
+echo "✅ Кэш почистили обновлён"
+
+
+# запуск команды sh deploy_server.sh
